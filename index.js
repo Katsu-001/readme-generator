52d13c72c148394397bf1a8f24631b0887861d46
Katsuyuki <katsuyuki.russell@gmail.com>
2023-07-27 15:45:01 -0500

    added array of questions to create readme with

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
-// TODO: Include packages needed for this application
+// needed packeages
+const fs = require('fs');
+const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title the project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Describe your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the steps for installation?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How would a user use your app?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Who contributed?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Test Instructions:',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'Choose license:',
+        choices: [
+            'Apache 2.0 License',
+            'Boost Software License 1.0',
+            'BSD 3-Clause License',
+            'The MIT License',
+            'The Do What the Fuck You Want to Public License',
+            'None'
+        ],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
