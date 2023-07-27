798fe6ef826e9f1dcf6b6c7c3d18e5e9a4187ecf
Katsuyuki <katsuyuki.russell@gmail.com>
2023-07-27 16:06:00 -0500

    added function to generate markdown file

---
+++ applicationREADME.md
@@ -0,0 +1,20 @@
+# Professional README Generator Starter Code
+
+[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+# JS ReadMe Generator
+
+## Description
+
+A simple command-line application using javascript to create a readme file using inquirer in the terminal.
+
+## Usage
+
+Follow through the prompts in the command-line to generate a markdown file for your readme.
+
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'None') {
@@ -14,7 +14,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'None') {
@@ -30,7 +30,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license) {
@@ -41,10 +41,36 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Testing
+
+  ${data.test}
+
+  ${renderLicenseSection(data.license)}
+  
+
+  ## Questions
+  
+  Feel free to contact me at:
+  * GitHub Username: ${data.github}
+  * Email: ${data.email}
 `;
 }
 
